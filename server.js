5ca6655ac2e6fa283aa636e54d3cf6d1cc9a5270
Nathan Geckle <nathan_geckle@yahoo.com>
2018-06-09 18:16:25 -0400

    Changed something

--- server.js
+++ server.js
@@ -18,11 +18,8 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/iflscienceArticles");
 
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/iflscienceArticles";
 
-// Set mongoose to leverage built in JavaScript ES6 Promises
-// Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
